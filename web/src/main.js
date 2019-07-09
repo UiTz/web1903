df9fdbf799fefafd5419d1ee3b858ec7f441cb8c
LiuJiaXin <liujiaxin988@qq.com>
2019-07-10 01:29:30 +0800

    开始配置后台文件

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入页面顶部加载进度条插件
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// 配置NProgress进度条选项  —— 动画效果
+NProgress.configure({ ease: 'ease', speed: 500 })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
 
 new Vue({
   router,
