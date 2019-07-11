e348ab5e95284031afad8222afdf2e960fa04a78
liujiaxin8 <35925178+liujiaxin8@users.noreply.github.com>
2019-07-12 03:14:51 +0800

    完成部分后台管理界面

---
+++ admin/src/views/Main.vue
@@ -0,0 +1,72 @@
+<template>
+  <el-container style="height: 100vh;">
+    <el-aside width="200px" style="background-color: rgb(84, 92, 100)">
+      <el-menu class="el-menu-vertical-demo" router :default-openeds="['1', '3']"
+               background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>内容管理</template>
+          <el-menu-item-group>
+            <template slot="title">分类</template>
+            <el-menu-item index="/categories/list">用户列表</el-menu-item>
+            <el-menu-item index="/categories/create">创建商品</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px; background-color: rgb(84, 92, 100)">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+      
+      <el-main>
+        <router-view></router-view>
+<!--        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+        </el-table>-->
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
+</script>
+
+<style scoped lang="less">
+  .el-header {
+    background-color: #B3C0D1;
+    color: #fff;
+    line-height: 60px;
+  }
+  .el-menu{
+    border:none;
+  }
+  .el-aside {
+    color: #333;
+  }
+</style>
