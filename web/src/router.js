88499d5418b0b866aece9115b0131fd1ad4a80d9
songbnb000 <1959693889@qq.com>
2019-07-15 20:56:58 +0800

    modify product.vue

--- web/src/router.js
+++ web/src/router.js
@@ -6,15 +6,9 @@ import index from './views/index.vue'
 import company from './views/company.vue'
 import Join from './views/Join.vue'
 import login from './components/login.vue'
-<<<<<<< HEAD
 import register from './components/register.vue'
 import productDetails from './components/productDetails.vue'
-=======
 import About from './views/About.vue'
-
->>>>>>> e1b180a0d9c54ce2f8e0d1f0f2e05f0e9e2a8fe9
-
-
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -28,14 +22,12 @@ export default new Router({
       component:company},
       {path: '/login',
       component:login},
-<<<<<<< HEAD
       {path: '/register',
       component:register},
      {path:'/productdetails',
-      component:productDetails}
-=======
-      {path: '/About',
+      component:productDetails},
+      {path:'/About',
       component:About},
->>>>>>> e1b180a0d9c54ce2f8e0d1f0f2e05f0e9e2a8fe9
+
   ]
 })
