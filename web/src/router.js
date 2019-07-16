f3f21df8074e16733fbde878adc6363719f6735d
songbnb000 <1959693889@qq.com>
2019-07-16 21:17:24 +0800

    modify product.vue

--- web/src/router.js
+++ web/src/router.js
@@ -6,21 +6,11 @@ import index from './views/index.vue'
 import company from './views/company.vue'
 import Join from './views/Join.vue'
 import login from './components/login.vue'
-<<<<<<< HEAD
-
-import register from './components/register.vue'
-import productDetails from './components/productDetails.vue'
-
-import About from './views/About.vue'
-
-
-
-
-=======
 import register from './components/register.vue'
 import productDetails from './components/productDetails.vue'
 import About from './views/About.vue'
->>>>>>> 88499d5418b0b866aece9115b0131fd1ad4a80d9
+import elflex from './views/elflex.vue'
+import Carousel from './views/Carousel.vue'
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -34,21 +24,15 @@ export default new Router({
       component:company},
       {path: '/login',
       component:login},
-<<<<<<< HEAD
-
-=======
->>>>>>> 88499d5418b0b866aece9115b0131fd1ad4a80d9
       {path: '/register',
       component:register},
      {path:'/productdetails',
       component:productDetails},
-<<<<<<< HEAD
-
-      {path: '/About',
-=======
       {path:'/About',
->>>>>>> 88499d5418b0b866aece9115b0131fd1ad4a80d9
       component:About},
-
+      {path:'/Elflex',
+      component:elflex},
+      {path:'/Carousel',
+      component:Carousel},
   ]
 })
