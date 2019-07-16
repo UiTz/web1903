c474aad42a482405fbe23d6d1079e28fae54e228
chenyang-1 <52656533+chenyang-1@users.noreply.github.com>
2019-07-16 19:19:07 +0800

    Update router.js

--- web/src/router.js
+++ web/src/router.js
@@ -6,21 +6,17 @@ import index from './views/index.vue'
 import company from './views/company.vue'
 import Join from './views/Join.vue'
 import login from './components/login.vue'
-<<<<<<< HEAD
 
 import register from './components/register.vue'
 import productDetails from './components/productDetails.vue'
-
 import About from './views/About.vue'
+import CompanyOne from './components/CompanyOne.vue'
+import CompanyTwo from './components/CompanyTwo.vue'
+import CompanyThree from './components/CompanyThree.vue'
+import CompanyFour from './components/CompanyFour.vue'
+import CompanyFive from './components/CompanyFive.vue'
+import CompanySix from './components/CompanySix.vue'
 
-
-
-
-=======
-import register from './components/register.vue'
-import productDetails from './components/productDetails.vue'
-import About from './views/About.vue'
->>>>>>> 88499d5418b0b866aece9115b0131fd1ad4a80d9
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -34,21 +30,24 @@ export default new Router({
       component:company},
       {path: '/login',
       component:login},
-<<<<<<< HEAD
-
-=======
->>>>>>> 88499d5418b0b866aece9115b0131fd1ad4a80d9
       {path: '/register',
       component:register},
      {path:'/productdetails',
       component:productDetails},
-<<<<<<< HEAD
-
-      {path: '/About',
-=======
       {path:'/About',
->>>>>>> 88499d5418b0b866aece9115b0131fd1ad4a80d9
       component:About},
+      {path:'/CompanyOne',
+      component:CompanyOne},
+      {path:'/CompanyTwo',
+      component:CompanyTwo},
+      {path:'/CompanyThree',
+      component:CompanyThree},
+      {path:'/CompanyFour',
+      component:CompanyFour},
+      {path:'/CompanyFive',
+      component:CompanyFive},
+      {path:'/CompanySix',
+      component:CompanySix},
 
   ]
 })
